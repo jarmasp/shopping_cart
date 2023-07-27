f1a2e44cb4f3a42df22b4fcf4ca6a59d78ba3661
Jose Armas <armasjose606@gmail.com>
2023-07-27 17:15:46 -0400

    Add: filters header

---
+++ shopping-cart/src/components/Header.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import {Filters} from './Filters.jsx'
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>React shop</h1>
+      <Filters/>
+    </header>
+  )
+}
