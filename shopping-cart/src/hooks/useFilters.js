92f03f8f8b7d0c3aaa2b0b68011e4d2ae03aed39
Jose Armas <armasjose606@gmail.com>
2023-07-27 17:16:09 -0400

    Add: useFilters Hook

---
+++ shopping-cart/src/hooks/useFilters.js
@@ -0,0 +1,19 @@
+import { FiltersContext } from "../context/filters.jsx"
+import { useContext } from "react"
+
+export const useFilters = () => {
+  const {filters, setFilters} = useContext(FiltersContext)
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice && 
+        (
+          filters.category === 'all' || 
+          product.category === filters.category
+        )
+      )
+    })
+  }
+  return {filterProducts, setFilters}
+}
