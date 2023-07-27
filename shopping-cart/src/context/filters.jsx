5000e6c7057740d6941319129a92c088d05c73e0
Jose Armas <armasjose606@gmail.com>
2023-07-27 17:14:33 -0400

    add: global context for filters

---
+++ shopping-cart/src/context/filters.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState } from 'react'
+
+export const FiltersContext = createContext()
+
+export const FiltersProvider = ({ children }) => {
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 250
+  })
+
+  return (
+    <FiltersContext.Provider value={{
+      filters,
+      setFilters
+    }}
+    >
+      {children}
+    </FiltersContext.Provider>
+  )
+}
